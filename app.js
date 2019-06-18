07b78a66d8bb147c7a9d22c956040321b3e4bff4
jacs <wbruce0110@gmail.com>
2019-06-18 21:57:34 +0800

    setup routes

--- app.js
+++ app.js
@@ -22,8 +22,44 @@ db.once('open', () => {
 const Todo = require('./models/todo')
 
 // routes 
+// index
 app.get('/', (req, res) => {
   res.send('Hello world!')
+})
+
+// list all todos
+app.get('/todos', (req, res) => {
+  res.send('list all todos')
+})
+
+// new todo creating page
+app.get('/todos/new', (req, res) => {
+  res.send('show todo creating page')
+})
+
+// show one todo page
+app.get('/todos/:id', (req, res) => {
+  res.send('show the detail of one todo')
+})
+
+// create a new todo
+app.post('/todos', (req, res) => {
+  res.send('create a new todo')
+})
+
+// todo editing page
+app.get('/todos/:id/edit', (req, res) => {
+  res.send('show todo editing page')
+})
+
+// edit a todo
+app.post('/todos/:id', (req, res) => {
+  res.send('Edit a todo')
+})
+
+// delete a todo
+app.post('/todos/:id/delete', (req, res) => {
+  res.send('delete a todo')
 })
 
 // listening on localhost
